9faa3ef4667bbabc03b030d5464be1312723740b
Andrea Gava <andrea@smartgap.it>
2012-12-02 13:57:12 +0100

    #Bugfix 3

--- js/jquery.FileManager.js
+++ js/jquery.FileManager.js
@@ -2,59 +2,58 @@ var move_in;
 var folder;
 
 function dragTree(selector) {
-	
-	$('.selected').each(function(){
+
+	$('.selected').each(function() {
 		$(this).removeClass('selected');
 	});
-	
+
 	$(selector).addClass('selected');
 	// FUNZIONE PER IL RENAME
-	/*
-	var timeoutId = 0;
-	
-	$('#myElement').mousedown(function() {
-	    timeoutId = setTimeout(myFunction, 1000);
-	}).bind('mouseup mouseleave', function() {
-	    clearTimeout(timeoutId);
-	});
-	*/
-	$(".selected").dblclick(function() {
-		var a = $(this);
-		var i = $(this).children("input");
-		var testo = a.html();
-		if (testo.indexOf("<input name=") > -1) {
-			var testo = i.attr("value");
-			a.html(testo);
-		} else {
-			var uniqid = new Date().getUTCMilliseconds();
-			a.html("<input name='" + testo + "' id='" + uniqid + "' value='" + testo + "' />");
-			$('#' + uniqid).focus();
-			$('#' + uniqid).blur(function() {
-				var a = $(this).parent();
-				var l = a.parent();
-				var testo = $(this).attr("value");
-				a.html(testo);
-			});
 
-		}
+	var timeoutId = 0;
+
+	$('.selected').mousedown(function() {
+		timeoutId = setTimeout(function() {
+			var a = $('.selected');
+			var i = a.children("input");
+			var testo = a.html();
+			var ext = testo.substr((testo.lastIndexOf('.') + 1));
+			var filename = testo.substr(0, testo.lastIndexOf('.')) || testo;
+			if (testo.indexOf("<input class=") > -1) {
+				var testo = i.attr("value");
+				a.html(testo);
+			} else {
+				var uniqid = new Date().getUTCMilliseconds();
+				a.html("<input class='rename' name='" + uniqid + "' id='" + uniqid + "' value='" + filename + "' />");
+				$('#' + uniqid).focus();
+				$('#' + uniqid).blur(function() {
+					var a = $(this).parent();
+					var testo = $(this).attr("value");
+					if (ext != testo) {
+						new_filename = testo + "." + ext;
+					} else {
+						new_filename = testo;
+					}
+					a.html(new_filename);
+				});
+
+			}
+		}, 1000);
+	}).bind('mouseup mouseleave', function() {
+		clearTimeout(timeoutId);
 	});
-	//END RENAME
-	
-	
+
+	// END RENAME
+
 	// DRAG & DROP
 	$(".selected").draggable({
-		appendTo : "body",
-		helper : "clone"
+		appendTo : "body", helper : "clone"
 	});
 	$('.dir').children('.filename').droppable({
-		activeClass : "ui-state-default",
-		hoverClass : "drop",
-		accept : ":not(.ui-sortable-helper)",
-		over : function(event, ui) {
+		activeClass : "ui-state-default", hoverClass : "drop", accept : ":not(.ui-sortable-helper)", over : function(event, ui) {
 			move_in = $('.drop').parent('li').attr('rel');
 			folder = $('.drop').html();
-		},
-		drop : function(event, ui) {
+		}, drop : function(event, ui) {
 			$(".edit").find(".filename").unbind("click");
 			var move_from = $('.selected').parent('li').attr('rel');
 			var file = $('.selected').html();
@@ -92,7 +91,7 @@ function dragTree(selector) {
 			});
 		}
 	});
-	
+
 	var selected_folder = $('.selected').parent().attr('rel');
 	var selected_file = $(selector).html();
 	console.log(selected_folder + selected_file);
